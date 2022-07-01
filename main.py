9ba96bdb7e08bfc22f60a89481f334de50c74df6
Lucas Vega <lucasvega02@gmail.com>
2022-06-30 23:24:04 -0300

    Finish the exercise

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import random
 
+ROWS = 20
+COLS = 12
+
 # FUNCTIONS
 # 1) Sales x Months' Matrix Creation
 matrix = []
-def create_matrix(n, m, value):
+def create_matrix(n, m):
 
   for i in range(n):
     matrix.append([])
     for j in range(m):
+      value =  random.randint(1, 100)
       matrix[i].append(value)
 
   return matrix
@@ -24,7 +28,7 @@ def fill_matrix():
   code = int(input("Enter the product's code: "))
 
   while(code != 0):
-    while(code < 1 or code > 20):
+    while(code < 1 or code > ROWS):
       code = int(input("ALERT! Enter a valid product's code: "))
 
     day = int(input("Enter the sale's day: "))
@@ -32,32 +36,85 @@ def fill_matrix():
       day = int(input("ALERT! Enter a valid number: "))
 
     month = int(input("Enter the sale's month: "))
-    while(month < 1 or month > 12):
+    while(month < 1 or month > COLS):
       month = int(input("ALERT! Enter a valid number (1 - 12): "))
 
     quant = int(input("Enter the sold's quantity: "))
     while(quant < 0):
       quant = int(input("ALERT! Enter a valid quantity: "))
 
-    matrix[0][0] = prices[0]*{quant}
+    matrix[code - 1][month - 1] += prices[code - 1] * quant
     
     code = int(input("Enter the product's code: "))
 
+# 4) Print sales per product, per month
+def sales_x_month():
+  for i in range(ROWS):
+    for j in range(COLS):
+      soldItems = int(matrix[i][j] / prices[i])
+      print(f"The product #{i +1} was sold {soldItems} times in the month {j + 1} ")
+
+# 5) Print the product with the biggest profit
+def max_profit():
+  max = matrix[0][0]
+  posRow = 0
+  posCol = 0
+  for i in range(ROWS):
+    for j in range(COLS):
+      if(matrix[i][j] > max):
+        max = matrix[i][j]
+        posRow = i
+        posCol = j
+  print(f"The product with the biggest profit was the product #{posRow + 1}, in the ({posRow +1}, {posCol + 1}) position, with a total of ${max}.")
+
+# 6) Profits per Quarter
+quarterProfits = []
+def quarter_profits():
+  quarter = 1
+  quarterFrom = 0 
+  quarterTo = 3
+
+  while(quarterTo <= COLS):
+    colArr = 0
+    for j in range(quarterFrom, quarterTo):
+      accCol = 0
+      for i in range(ROWS):
+        accCol += matrix[i][j]
+      colArr += accCol
+
+    quarterProfits.append(colArr)
+
+    print(f"The {quarter}º quarter has a total of ${colArr} per month")
+    quarterFrom += 3
+    quarterTo += 3
+    quarter += 1
+
+  print(quarterProfits)
+
+# 7) Print the quarter with less profits
+def less_profit():
+  min = quarterProfits[0]
+  pos = 0
+  for i in range(len(quarterProfits)):
+    if(quarterProfits[i] < min):
+      min = quarterProfits[i]
+      pos = i
+  print(f"The quarter with less profits is the {pos + 1}º with a total of ${min}")
+
 # Prices Array with its prices (Product code is relative to the arr position)
 prices = []
-for x in range(20):
+for x in range(ROWS):
   #value = int(input(f"Enter the price of the item #{x + 1}: "))
   value = int(random.randint(100, 500))
   prices.append(value)
 
-# Print each product code with its price
-#for x in range(len(prices)):
-  #print(f"The product #{x + 1} costs ${prices[x]}")
-
+# Main Execution
 print(prices)
-create_matrix(20, 12, 0)
-show_matrix(matrix)
-fill_matrix()
+create_matrix(ROWS, COLS)
+#fill_matrix()
+
 show_matrix(matrix)
 
-# Informing: Details for each product for each month, product with more profits, quarter with less profits
+max_profit()
+quarter_profits()
+less_profit()
